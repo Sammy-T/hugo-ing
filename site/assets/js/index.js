ce6820be69653dd694a03d867090ee444d45ee04
Sammy-T <cyam9019@gmail.com>
2025-05-01 22:40:28 -0400

    Migrate to animejs v4 syntax

--- site/assets/js/index.js
+++ site/assets/js/index.js
@@ -1,27 +1,27 @@
 // Include external JavaScript
-const anime = require('animejs');
+import { animate, utils } from 'animejs';
 
 // Add your JavaScript
 function animateBoardBg() {
-    anime({
-        targets: '.billboard',
-        backgroundImage: function() {
-            const r = anime.random(50, 150);
-            const g = anime.random(50, 150);
-            const b = anime.random(50, 150);
-            const color1 = `rgb(${r}, ${g}, ${b})`;
+    animate('.billboard', 
+        {
+            backgroundImage: function() {
+                const r = utils.random(50, 150);
+                const g = utils.random(50, 150);
+                const b = utils.random(50, 150);
+                const color1 = `rgb(${r}, ${g}, ${b})`;
 
-            const r2 = anime.random(50, 150);
-            const g2 = anime.random(50, 150);
-            const b2 = anime.random(50, 150);
-            const color2 = `rgb(${r2}, ${g2}, ${b2})`;
+                const r2 = utils.random(50, 150);
+                const g2 = utils.random(50, 150);
+                const b2 = utils.random(50, 150);
+                const color2 = `rgb(${r2}, ${g2}, ${b2})`;
 
-            return `linear-gradient(45deg, ${color1}, ${color2})`;
-        },
-        easing: 'easeInOutSine',
-        duration: 3000,
-        complete: animateBoardBg
-    });
+                return `linear-gradient(45deg, ${color1}, ${color2})`;
+            },
+            ease: 'easeInOutSine',
+            duration: 3000,
+            onComplete: animateBoardBg
+        });
 }
 
 console.log('Edit \'site/assets/js/index.js\' with your JavaScript.');
